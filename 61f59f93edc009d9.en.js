5482fe3e8f22db5d20f1e01fe5c6242deb697fb8
youssef200223 <youssefzouina0@gmail.com>
2024-11-23 00:22:24 +0100

    Add files via upload

---
+++ 61f59f93edc009d9.en.js
@@ -0,0 +1,2 @@
+window['cmsg']['assets'] = window['cmsg']['assets'] || {};
+window['cmsg']['assets']["en"] = Object.assign(window['cmsg']['assets']["en"] || {}, {"5706":{"js":["632a778c434a80eb.strings.js"],"css":[]},"8701":{"js":["3aacdf3076e2b21b.strings.js"],"css":[]}});
